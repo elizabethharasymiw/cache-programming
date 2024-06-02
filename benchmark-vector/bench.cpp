70dfb07904038001ebbe7c4a2c1ada1e02c9da9b
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-06-02 00:07:50 -0500

    Test old methods to pin down c++ optimizer

--- benchmark-vector/bench.cpp
+++ benchmark-vector/bench.cpp
@@ -1,6 +1,14 @@
 #include <benchmark/benchmark.h>
 #include <vector>
 
+static void escape(void *p){
+  asm volatile("" : : "g"(p) : "memory");
+}
+
+static void clobber(){
+  asm volatile("" : : : "memory");
+}
+
 static void bench_create(benchmark::State &state){
   while(state.KeepRunning()) {
     std::vector<int> v;
@@ -25,5 +33,34 @@ static void bench_push_back(benchmark::State &state){
   }
 }
 BENCHMARK(bench_push_back);
+
+static void bench_create_pinned(benchmark::State &state){
+  while(state.KeepRunning()) {
+    std::vector<int> v;
+    escape(&v);
+    (void) v;
+  }
+}
+BENCHMARK(bench_create_pinned);
+
+static void bench_reserve_pinned(benchmark::State &state){
+  while(state.KeepRunning()) {
+    std::vector<int> v;
+    v.reserve(1);
+    escape(v.data());
+  }
+}
+BENCHMARK(bench_reserve_pinned);
+
+static void bench_push_back_pinned(benchmark::State &state){
+  while(state.KeepRunning()) {
+    std::vector<int> v;
+    v.reserve(1);
+    escape(v.data());
+    v.push_back(42);
+    clobber();
+  }
+}
+BENCHMARK(bench_push_back_pinned);
 
 BENCHMARK_MAIN();
